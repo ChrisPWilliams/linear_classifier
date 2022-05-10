6c06830a0995a1a2ae291692777127872388b9c1
Chris Williams <orktagonapus@gmail.com>
2022-05-10 21:21:13 +0100

    Generate and display fake data

--- linear_classifier.py
+++ linear_classifier.py
@@ -2,4 +2,21 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-a = tf.constant()
+dataset_size = 2000
+
+a = np.random.multivariate_normal(
+    mean=[2,0],
+    cov=[[1,0.5],[0.5,1]],
+    size=dataset_size//2)
+
+b = np.random.multivariate_normal(
+    mean=[-2,0],
+    cov=[[0.5,1],[1,0.5]],
+    size=dataset_size//2)
+
+inputs = np.vstack((a,b)).astype("float32")
+
+labels = np.vstack((np.ones((dataset_size//2,1), dtype="float32"), np.zeros((dataset_size//2,1), dtype="float32")))
+
+plt.scatter(inputs[:,0], inputs[:,1], c=labels[:,0])
+plt.show()
